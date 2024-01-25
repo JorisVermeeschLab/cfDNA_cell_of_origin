11843f03de0e6713abacfbfeccdb3c35fda833a3
Kate E Stanley <91881379+katestan@users.noreply.github.com>
2024-01-25 17:17:06 +0100

    Update differentialCellTypeRanking.R

--- case_control/differentialCellTypeRanking.R
+++ case_control/differentialCellTypeRanking.R
@@ -8,5 +8,6 @@ library(ggplot2)
 args <- commandArgs(TRUE)
 
 files=args[1]
+comparisons=[2]
 
-#Read in a sample file containing all study samples with a unique sample ID as the first column. The second column should be a path to the correlation.csv output from the https://github.com/shendurelab/cfDNA gene expression analysis  
+#Read in a sample file containing all unique study sample IDs as the first column. The second column should be a path to the correlation.csv output from the https://github.com/shendurelab/cfDNA gene expression analysis for that sample. The gene expression analysis should be performed using average gene expression values summarised by cell type per tissue from the Tabula Sapiens consortium.     
