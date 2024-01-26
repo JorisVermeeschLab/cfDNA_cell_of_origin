9e33669deb0cee10d09e758703744f4a1f0abc25
Kate E Stanley <91881379+katestan@users.noreply.github.com>
2024-01-26 11:59:09 +0100

    Update plotWalktrapTsne.R

--- case_control/plotWalktrapTsne.R
+++ case_control/plotWalktrapTsne.R
@@ -19,6 +19,8 @@ walkstep <- args[5]
 myseed <- args[6]
 
 #pivot wider combined data file
-combined.data <- fread(paste0(output_dir, "/combined_data.txt"), header=T)
-combined.data <- combined.data %>% select(cell_type, sample, rank, class)
-combined.data$group <- paste0(combined.data$sample, ":", combined.data$class)
+combined <- fread(paste0(output_dir, "/combined_data.txt"), header=T)
+combined$group <- paste0(combined$sample, ":", combined$status)
+combined.wider <- combined %>% select(-sample, -status) %>% pivot_wider(names_from = cell_type, values_from = rank) %>% as.data.frame()
+rownames(combined.wider) <- combined.wider$group
+combined.wider <- combined.wider %>% select(-group) %>% data.matrix()
