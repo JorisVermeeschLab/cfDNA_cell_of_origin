72ef3fca3380557debde698111fda4b30d451b2c
Kate E Stanley <91881379+katestan@users.noreply.github.com>
2024-01-26 12:58:12 +0100

    Update plotWalktrapTsne.R

--- case_control/plotWalktrapTsne.R
+++ case_control/plotWalktrapTsne.R
@@ -11,18 +11,18 @@ library(FNN)
 library(Matrix)
 library(cowplot)
 
-output_dir <- args[1]
+outputdir <- args[1]
 outprefix <- args[2]
 perplexity <- args[3]
 initk <- args[4]
-walkstep <- args[5]
+walksteps <- args[5]
 myseed <- args[6]
 
 #pivot wider combined data file
-combined <- fread(paste0(output_dir, "/combined_data.txt"), header=T)
+combined <- fread(paste0(outputdir, "/combined_data.txt"), header=T)
 combined$group <- paste0(combined$sample, ":", combined$status)
 combined.wider <- combined %>% select(-sample, -status) %>% pivot_wider(names_from = cell_type, values_from = rank) %>% as.data.frame()
 rownames(combined.wider) <- combined.wider$group
 combined.wider <- combined.wider %>% select(-group) %>% data.matrix()
 
-plotDifferentialCellTypes(combined.wider, )
+plotDifferentialCellTypes(dtmat, myseed, initk, walksteps, perplexity, output_dir)
