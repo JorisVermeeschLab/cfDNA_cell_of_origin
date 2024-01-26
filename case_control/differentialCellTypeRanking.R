a7a2b4f6e4875be0d9b11cc1063e6108d1b05e4e
Kate E Stanley <91881379+katestan@users.noreply.github.com>
2024-01-26 11:21:47 +0100

    Update differentialCellTypeRanking.R

--- case_control/differentialCellTypeRanking.R
+++ case_control/differentialCellTypeRanking.R
@@ -1,25 +1,23 @@
 library(data.table)
-library(rstatix)
 library(tidyverse)
-library(ggrepel)
 library(rstatix)
+library(ggrepel)
 library(ggplot2)
 
 args <- commandArgs(TRUE)
 
-files=args[1]
+corr_files=args[1] 
 comparisons=[2]
-output_data=[3]
-output_plot=[4]
+output_dir=[3]
 
-#import sample file containing unique study sample IDs as the first column ("sample") and the path to the correlation.csv output for each sample in the second column ("path").
-files <- fread(files, header=T)
+#import sample file containing unique study sample IDs and the path to the correlation.csv output for each sample.
+corr_files <- fread(files, header=T)
 
-#import annotation file containing unique sample ID in the first column ("sample") and case-control status in the second column ("status").
+#import annotation file containing unique sample ID and the case or control status for each sample.
 comparisons <- fread(comparisons, header=T)
 
 #import correlation.csv outputs for all samples
-dat <- lapply(files$path, function(x) fread(x, header=T))
+dat <- lapply(corr_files$path, function(x) fread(x, header=T))
 
 #extract cell type column, annotate with unique sample ID, and generate rank order.
 for (i in 1:length(dat)){
@@ -35,7 +33,8 @@ dat <- do.call(rbind, dat)
 #annotate combined output
 dat.anno <- left_join(comparison, dat)
 dat.anno$status <- as.factor(dat.anno$status)
-
+write.table(dat, paste0(output_dir, "/combined_data.txt"), col.names=T, row.names=F, quote=F, sep="\t")
+              
 #compare ranks between cases and controls for each cell type
 allcomp <- c()
 cell <- unique(dat.anno$cell_type)
@@ -46,7 +45,7 @@ for(i in 1:length(cell)) {
   labels <- paste0(pheno.counts[1,1],".n.", pheno.counts[1,2], ".vrs.", pheno.counts[2,1], ".n.", pheno.counts[2,2])
   print(labels)
   print(paste0("reference group = ", pheno.counts[2,1]$V1))
-  df.wilcox <- wilcox_test(df, rank ~ status, ref.group = pheno.counts[2,1]$V1, p.adjust.method="fdr")
+  df.wilcox <- wilcox_test(df, rank ~ status, ref.group = pheno.counts[2,1]$V1)
   df.mean <- df %>% group_by(status) %>% summarize(Mean = mean(rank, na.rm=TRUE)) %>% as.data.frame()
   df.mean.FC <- df.mean$Mean[2]/df.mean$Mean[1] %>% as.data.frame()
   colnames(df.mean.FC) <- c("foldchange")
@@ -62,7 +61,7 @@ allcomp$p.adj <- p.adjust(allcomp$p, method="fdr")
 allcomp <- allcomp[order(allcomp[,10]),]
 
 #export output
-write.table(alltruegroup, paste0(output_data, "/", labels, ".txt"), sep="\t", col.names=T, row.names=F, quote=F)
+write.table(allcomp, paste0(output_dir, "/", labels, ".txt"), sep="\t", col.names=T, row.names=F, quote=F)
 
 #create color and labels for volcano plot
 allcomp$diffranked <- "Not Sig"
@@ -83,5 +82,5 @@ ggplot(data=allcomp, aes(x=log2(foldchange), y=-log10(p), color=diffranked, labe
     geom_label_repel(size=2)+
     scale_color_manual(values = mycolors)+
     theme_classic() + ggtitle(labels)
-ggsave(paste0(output_plot, "/", labels, ".pdf"), height=7, width=7)
+ggsave(paste0(output_dir, "/", labels, ".pdf"), height=7, width=7)
               
