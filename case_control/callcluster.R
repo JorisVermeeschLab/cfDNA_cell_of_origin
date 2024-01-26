45498132133e9e5c2f01f2165d355ef5d9fc163b
Kate E Stanley <91881379+katestan@users.noreply.github.com>
2024-01-26 13:25:49 +0100

    Update and rename plotWalktrapTsne.R to callcluster.R

--- case_control/callcluster.R
+++ case_control/callcluster.R
@@ -1,28 +1,24 @@
 library(data.table)
-library(reshape2)
 library(Rtsne)
-library(parallel)
 library(ggplot2)
-library(RColorBrewer)
 library(ggrepel)
 library(dplyr)
 library(igraph)
 library(FNN)
-library(Matrix)
 library(cowplot)
 
-outputdir <- args[1]
+output_dir <- args[1]
 perplexity <- args[3]
 initk <- args[4]
 walksteps <- args[5]
 myseed <- args[6]
 
 #pivot wider combined data file
-combined <- fread(paste0(outputdir, "/combined_data.txt"), header=T)
+combined <- fread(paste0(output_dir, "/combined_data.txt"), header=T)
 combined$group <- paste0(combined$sample, ":", combined$status)
 combined.wider <- combined %>% select(-sample, -status) %>% pivot_wider(names_from = cell_type, values_from = rank) %>% as.data.frame()
 rownames(combined.wider) <- combined.wider$group
 combined.wider <- combined.wider %>% select(-group) %>% data.matrix()
 
 set.seed(myseed)
-plotWalkTrapClusterTsne(dtmat, initk, walksteps, perplexity, output_dir)
+plotWalkTrapClusterTsne(combined.wider, initk, walksteps, perplexity, output_dir)
