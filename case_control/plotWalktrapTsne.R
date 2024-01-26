cd3756f37ad30d2186e2df52e8bb88a2e73c1103
Kate E Stanley <91881379+katestan@users.noreply.github.com>
2024-01-26 11:40:21 +0100

    Update plotWalktrapTsne.R

--- case_control/plotWalktrapTsne.R
+++ case_control/plotWalktrapTsne.R
@@ -11,7 +11,7 @@ library(FNN)
 library(Matrix)
 library(cowplot)
 
-outputdir <- args[1]
+output_dir <- args[1]
 outprefix <- args[2]
 perplexity <- args[3]
 initk <- args[4]
@@ -19,6 +19,6 @@ walkstep <- args[5]
 myseed <- args[6]
 
 #pivot wider combined data file
-combined.data <- fread(paste0(outputdir, "/combined_data.txt"), header=T)
+combined.data <- fread(paste0(output_dir, "/combined_data.txt"), header=T)
 combined.data <- combined.data %>% select(cell_type, sample, rank, class)
 combined.data$group <- paste0(combined.data$sample, ":", combined.data$class)
